447445995fabcfc40e2f279217b402e3145ac4bc
lmalki-h <lmalki-h@student.42.fr>
2022-01-13 23:18:46 +0100

    wip

--- TP1/ex01/main.c
+++ TP1/ex01/main.c
@@ -10,8 +10,8 @@ void main(void)
 
 	TCCR1B |= (1 << CS12); //set 0-2 bits of TCCR1B to prescalar. see: 16.11.2 & Table 16-5.
 	
-	ICR1 = F_CPU / 256 - 1;
+	ICR1 = F_CPU / 256 - 1; //
 
-	OCR1A = ICR1 / 2;
+	OCR1A = ICR1 / 10;
 	for (;;) ;
 }
