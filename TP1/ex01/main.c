7c944e5fee844ad767633e519867c1eea0b0aeaa
event <event@e2r3p4.clusters.42paris.fr>
2022-01-14 17:42:48 +0100

    done

--- TP1/ex01/main.c
+++ TP1/ex01/main.c
@@ -1,17 +1,23 @@
 #include <avr/io.h>
 
+//using fast-PWM
 void main(void)
 {
-	DDRB |= ( 1 << PB1) ; //set PB1 to output mode
-	TCCR1A = 0;
-	TCCR1B = 0;
-	TCCR1A |= (1 << COM1A0);
-	TCCR1B |= ( 1 << WGM12);
-
-	TCCR1B |= (1 << CS12); //set 0-2 bits of TCCR1B to prescalar. see: 16.11.2 & Table 16-5.
+	DDRB |= ( 1 << PB1) ; //set PB1 to output mode see 16.8
 	
-	ICR1 = F_CPU / 256 - 1; //
+	TCCR1A |= (1 << COM1A1); //clear OC1A 16.11.1
+	
+	TCCR1A |= (1 << WGM11 ); //set Fast PWM mode 14 TOP=ICR1 16.11.1
+	TCCR1B |= ( 1 << WGM12) | ( 1 << WGM13); //set Fast PWM mode 14 TOP=ICR1 16.11.1
 
-	OCR1A = ICR1 / 10;
+
+	ICR1 = F_CPU / 256 - 1; //set TOP. comparison happens at the next cycle 16.9.3
+
+
+	TCCR1B |= (1 << CS12); //256. set 0-2 bits of TCCR1B to prescalar. see: 16.11.2 & Table 16-5.
+	
+
+	OCR1A = ICR1 / 10; //rapport cyclique de 50%
+
 	for (;;) ;
 }
